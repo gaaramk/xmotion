84b74d0829b5a6c3e99f2f6c79b1157228e3a33b
Mohammad Kamal El-Dein <gaarakml@gmail.com>
2025-07-01 06:40:34 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
 import ExercisesDetails from "./pages/ExercisesDetails";
 import Home from "./pages/Home";
 import Layout from "./layout/Layout";
@@ -7,33 +8,23 @@ import Contact from "./pages/Contact";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 
-const App = () => {
-  // Routes with basename for GitHub Pages
-  const routes = createBrowserRouter(
-    [
-      {
-        path: "/",
-        element: <Layout />,
-        children: [
-          { index: true, element: <Home /> },
-          { path: "contact", element: <Contact /> },
-          { path: "about", element: <About /> },
-          { path: "exercisesDetails/:id", element: <ExercisesDetails /> },
-          {
-            path: "*",
-            element: <NotFound />,
-          },
-        ],
-      },
+const queryClient = new QueryClient();
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "contact", element: <Contact /> },
+      { path: "about", element: <About /> },
+      { path: "exercisesDetails/:id", element: <ExercisesDetails /> },
+      { path: "*", element: <NotFound /> },
     ],
-    {
-      basename: "/xmotion",
-    }
-  );
+  },
+]);
 
-  // React Query
-  const queryClient = new QueryClient();
-
+const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={routes} />
